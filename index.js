c5e96439d9ebc0d0097d56b8ff144eacbbab6a86
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-08-17 01:42:30 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser = "Bob21"
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
